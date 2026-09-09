1d40adf1a32ca3bb2a500a5b8eec297896ed05b2
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2026-09-09 12:41:05 +0530

    fix: update dotenv usage in drizzle.config and package.json for environment variable management

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,13 +1,10 @@
-import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
-
-const env = config().parsed;
 
 export default defineConfig({
     schema: "./src/schema/**/*.ts",
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
-        url: env?.DATABASE_URL!,
+        url: process.env.DATABASE_URL!,
     },
 });
